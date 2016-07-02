38f85c8d65f7b48b04d3bd4bbf742147e2d79896
Timothy Conkle <dconkle@Timothys-MBP.fios-router.home>
2016-07-01 23:38:06 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,50 @@
+var answer = 0;
+var count = 0;
+
+function ranNum(){
+    var genNum = Math.floor((Math.random() * 100) + 1);
+    return genNum;
+}
+
+function feedBack(correctAnswer, guess){
+    var distance = Math.abs(correctAnswer - guess);
+    if (distance >= 50){
+        $('#feedback').text("Very Cold");
+    } else if (distance > 20 && distance < 50){
+        $('#feedback').text("Cold");
+    } else if (distance > 10 && distance <= 20){
+        $('#feedback').text("Hot");
+    }else if (distance != 0){
+        $('#feedback').text("Very Hot");
+    } else {
+        $('#feedback').text("Yay, You Win!");
+    }
+
+    return (distance == 0);
+}
+
+function getInput(){
+    var userGuess = $('#userGuess').val();
+    return parseInt(userGuess);
+}
+
+function newGame() {
+    answer = ranNum();
+}
+
+function guessButton() {
+    var guess = getInput();
+    feedBack(answer, guess);
+    count++;
+    $('#count').text(count);
+    $('ul#guessList').append("<li>" + guess + "</li>");
+}
 
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -12,6 +52,15 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    $('#newGame').click(function () {
+        newGame();
+    });
+
+    $('#guessButton').click(guessButton);
+
+    $('form').keypress(function(e) {
+        if (e.keyCode == 13) {
+            guessButton();
+        }
+    })
 });
-
-
